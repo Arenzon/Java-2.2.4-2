d6a4b90ad988efd850a64122cc049858ebc051f6
Natalya <76731572+Arenzon@users.noreply.github.com>
2021-01-25 23:31:44 +0300

    Update BodyMassIndex.java

--- BodyMassIndex.java
+++ BodyMassIndex.java
@@ -1,6 +1,7 @@
-public class BodyMassIndex {
-    public long calculate( int weight,int growt) {
-       int index = weight / growt * growt;
-        return index;
+public class Main {
+    public static void main(String[] args) {
+        BodyMassIndex service = new BodyMassIndex();
+       float index = service.calculate(180, 70);
+        System.out.println("Индекс массы тела " + index);
     }
 }
