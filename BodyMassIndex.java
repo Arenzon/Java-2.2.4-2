5d6b20395c3eea9c73c8767876b1cd3193e48d42
Natalya <76731572+Arenzon@users.noreply.github.com>
2021-01-26 00:29:04 +0300

    Update BodyMassIndex.java

--- BodyMassIndex.java
+++ BodyMassIndex.java
@@ -1,6 +1,6 @@
 public class BodyMassIndex {
     public float calculate( float weight,float growth) {
-       float index = weight / (growth * growth) * 100;
-        return index;
+    float index = weight / (growth * growth) * 100;
+    return index;
     }
 }
