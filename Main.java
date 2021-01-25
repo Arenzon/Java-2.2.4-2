4bf0528ab08148199d85bfd3a1fc4f5ba9c7215d
Natalya <76731572+Arenzon@users.noreply.github.com>
2021-01-25 23:32:47 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int growt = 180;
-        int weight = 70_000;
-        int index = weight / growt*growt;
+        BodyMassIndex service = new BodyMassIndex();
+       float index = service.calculate(180, 70);
         System.out.println("Индекс массы тела " + index);
     }
 }
